fe22fd92a497ac5a22b315fbea0d198e8eeec63d
najeehug <112579968+najeehug@users.noreply.github.com>
2025-12-03 21:07:58 -0500

    Add files via upload

---
+++ OLED_screen_test.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 20);
+  display.println("HELLO!");
+  display.display();
+}
+
+void loop() {}
